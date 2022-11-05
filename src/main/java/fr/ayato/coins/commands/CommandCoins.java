cd7e8be45f5f2559238321bc65016de0b71b5a7c
Louis <72603854+Ayatooo@users.noreply.github.com>
2022-11-05 15:53:53 +0100

    :zap;

--- src/main/java/fr/ayato/coins/commands/CommandCoins.java
+++ src/main/java/fr/ayato/coins/commands/CommandCoins.java
@@ -1,6 +1,7 @@
 package fr.ayato.coins.commands;
 
 import fr.ayato.coins.coin.Coin;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,11 +12,11 @@ public class CommandCoins implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
         if (args.length == 3) {
             if (args[1].equals("fragment")) {
-                Player p = (Player) sender;
-                p.getInventory().addItem(Coin.getCoinFragment(Integer.parseInt(args[2])));
+                final Player player = Bukkit.getPlayer(args[0]);
+                player.getInventory().addItem(Coin.getCoinFragment(Integer.parseInt(args[2])));
             } else if (args[1].equals("coin")) {
-                Player p = (Player) sender;
-                p.getInventory().addItem(Coin.getCoin(Integer.parseInt(args[2])));
+                final Player player = Bukkit.getPlayer(args[0]);
+                player.getInventory().addItem(Coin.getCoin(Integer.parseInt(args[2])));
             }
         } else {
             sender.sendMessage("§cLa commande est /coins <pseudo> <coin/fragment> <nombre>");
